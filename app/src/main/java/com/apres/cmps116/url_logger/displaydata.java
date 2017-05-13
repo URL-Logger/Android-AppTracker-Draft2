fd2fa23997a1405a21ee49d66f7a9fb9dc34e207
Darius Sakhapour <dsakhapo@gmail.com>
2017-05-13 13:40:24 -0700

    Rearranged UI, added extras

--- app/src/main/java/com/apres/cmps116/url_logger/displaydata.java
+++ app/src/main/java/com/apres/cmps116/url_logger/displaydata.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 /**
@@ -20,6 +21,7 @@ public class displaydata extends AppCompatActivity {
     ToggleButton statsBtn;
     public static final String PREFRENCES_FILE_NAME = "MyAppPreferences";
     Intent serviceIntent;
+    TextView tracking_indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,27 +36,31 @@ public class displaydata extends AppCompatActivity {
 
         serviceIntent = new Intent (displaydata.this, MyService.class);
         statsBtn = (ToggleButton) findViewById(R.id.stats_btn);
+        tracking_indicator = (TextView) findViewById(R.id.tracking_indicator);
         /*
         Toggle the button if it was previously toggled before the app closed
          */
         if(LoginActivity.loginSettings.getBoolean("On", false)){
-            statsBtn.setTextOn("On");
+            statsBtn.setTextOn("Off");
             statsBtn.setChecked(true);
+            tracking_indicator.setText("ON");
             startService(serviceIntent);
         }
         statsBtn.setOnClickListener(new View.OnClickListener() { //Logic for toggle button
             @Override
             public void onClick(View v) {
                 if (statsBtn.isChecked()){ //if On
-                    statsBtn.setTextOn("On");
+                    statsBtn.setTextOn("Off");
                     statsBtn.setChecked(true);
+                    tracking_indicator.setText("ON");
                     LoginActivity.editor.putBoolean("On", true);
                     LoginActivity.editor.commit();
                     startService(serviceIntent);
                 }
                 else{ //if Off
-                    statsBtn.setTextOff("Off");
+                    statsBtn.setTextOff("On");
                     statsBtn.setChecked(false);
+                    tracking_indicator.setText("OFF");
                     LoginActivity.editor.putBoolean("On", false);
                     LoginActivity.editor.commit();
                     stopService(serviceIntent);
